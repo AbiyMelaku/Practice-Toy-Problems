d0d1e713acc078798172dbac6da5357cc558aa4f
Abiy Melaku <agirma08@gmail.com>
2017-08-17 12:43:28 -0700

    Minor editing

--- LeetCode/TwoSum(EASY).js
+++ LeetCode/TwoSum(EASY).js
@@ -36,8 +36,8 @@ var twoSum = function(nums, target) {
 var twoSum = function(nums, target){
     var saved = {};
     var result = [];
-    for(i=0; i< nums.length; i++){
-      if(saved.hasOwnProperty(nums[i])){
+    for(var i = 0; i< nums.length; i++){
+      if( saved.hasOwnProperty(nums[i]) ){
         result[0] = saved[nums[i]] + 1;
         result[1] = i + 1;
         return result;
