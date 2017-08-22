26a67c5b1f3691c309eea62610b6ffdb728cf5ae
Abiy Melaku <agirma08@gmail.com>
2017-08-22 10:44:20 -0700

    alternate solution

--- LeetCode/romanToInteger(EASY).js
+++ LeetCode/romanToInteger(EASY).js
@@ -21,4 +21,33 @@ const romanToInt = (s) => {
 };
 
 
-console.log(romanToInt('XIV'));
+
+//ALTERNATE, slower solution
+
+var characterToNumber = function(character){
+    switch(character){
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+
+var romanToInt = function(s) {
+    var result = 0;
+    
+    for(var i = 0; i < s.length; i++){
+        if( i > 0 && (characterToNumber(s[i]) > characterToNumber(s[i - 1])) ){
+            result -= 2 * characterToNumber(s[i - 1]); // because previously added [!!!]
+        }
+        
+        result += characterToNumber(s[i]);
+    }
+    
+    return result;
+};
