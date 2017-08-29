fb81e506006fab9053479420d3ba97d21681509c
Abiy Melaku <agirma08@gmail.com>
2017-08-29 14:56:25 -0700

    reverse words in a string III easy letcode

---
+++ LeetCode/reverseWordsInAStringIII(EASY).js
@@ -0,0 +1,20 @@
+var reverseWords = function(s) {
+    s = s.split(' ');
+    var result = '';
+
+    for(var j = 0; j < s.length; j++){
+        var res = '';
+        for(var i = s[j].length - 1; i >= 0 ; i--){
+            res += s[j][i];
+        }
+        result += ' ' + res;
+    }
+    return result.slice(1);
+};
+
+
+
+//alternate solution
+var reverseWords = function(s) {
+    return s.split('').reverse().join('').split(' ').reverse().join(' ');
+}
