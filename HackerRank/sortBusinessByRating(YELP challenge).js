0e952a63106de58d64e86166cd46efb6e7381713
Abiy Melaku <agirma08@gmail.com>
2017-09-05 21:32:19 -0700

    yelp challenge for new grad software engineer role

---
+++ HackerRank/sortBusinessByRating(YELP challenge).js
@@ -0,0 +1,49 @@
+function sortBusinessesByRating(businesses) {
+    /*
+    Array of businesses info where each element is a object containing id
+    and rating. Sort the businesses by their rating in decreasing order.
+    Preserve the original ordering if two businesses have same rating.
+
+    Args:
+        businesses: Array of businesses info where business info is
+        a object containing id and rating.
+
+    Return:
+        Array of businesses' info
+
+    Example:
+
+    Input:
+    [
+        {'id': 101, rating: 5.0},
+        {'id': 102, rating: 2.0},
+        {'id': 103, rating: 3.0},
+        {'id': 104, rating: 5.0},
+        {'id': 105, rating: 5.0},
+    ]
+
+    Output:
+    [
+        {'id': 101, rating: 5.0},
+        {'id': 104, rating: 5.0},
+        {'id': 105, rating: 5.0},
+        {'id': 103, rating: 3.0},
+        {'id': 102, rating: 2.0},
+    ]
+    */
+
+    // TODO: COMPLETE ME
+    var swapped;
+    do{
+        swapped = false;
+        for( var i = 0; i < businesses.length - 1; i++){
+            if( businesses[i].rating < businesses[i + 1].rating ){
+                var temp = businesses[i];
+                businesses[i] = businesses[i + 1];
+                businesses[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return businesses;
+}
